66b0dadd5a123dbfbe497f4d187243e028caaacd
dhanavishnu13 <vishnukvka@gmail.com>
2023-10-26 10:45:59 +0530

    updated authendication UI

---
+++ db/migrate/20231025125345_add_status_toexpenses.rb
@@ -0,0 +1,4 @@
+class AddStatusToexpenses < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_13_154610) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_25_130406) do
   create_table "expenses", force: :cascade do |t|
     t.string "payee_name"
     t.string "category"
@@ -18,6 +18,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_13_154610) do
     t.string "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_expenses_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +35,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_13_154610) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "expenses", "users"
 end
